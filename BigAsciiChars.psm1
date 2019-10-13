2ddb451577d9f9de9646f558c6628126036ce0a3
David Johnson <13346199+Cirzen@users.noreply.github.com>
2019-10-13 23:06:14 +0100

    WIP of conversion to class based font

--- BigAsciiChars.psm1
+++ BigAsciiChars.psm1
@@ -144,6 +144,202 @@ $Codes = @{
     128 = 150223247  # "€"
 }
 
+# Represents the int64 value and width of a given character
+class CharInfo
+{
+    [int64]$Value
+    [int]$Width
+
+    CharInfo([int64]$value, [bool]$SkipWidthCalc = $false)
+    {
+        $this.Value = $value
+        $this.Width = if ($SkipWidthCalc) {0} else {GetCharWidth} #Update
+    }
+    CharInfo([int64]$value, [int]$width)
+    {
+        $this.Value = $value
+        $this.Width = $width
+    }
+}
+
+
+# Base class for any derived fonts
+class FontBase
+{
+    [System.Collections.Generic.Dictionary[int,CharInfo]]$Codes
+    [int]$Width
+    [int]$Height
+
+    FontBase([hashtable]$codes, [int]$width, [int]$height)
+    {
+        $this.Codes      = [System.Collections.Generic.Dictionary[int,CharInfo]]::new()
+        foreach ($key in $codes.Keys)
+        {
+            $this.Codes.Add($key, [CharInfo]::new($codes[$key], $true))
+        }
+        $this.CharWidth  = $width
+        $this.CharHeight = $height
+    }
+
+    [bool]IsCharSupported([char]$c)
+    {
+        return $this.Codes.ContainsKey([int]$c)
+    }
+
+    [CharInfo]GetCharInfo([char]$c)
+    {
+        if (!$this.IsCharSupported($c))
+        {
+            return [CharInfo]::new((GetUnknownSymbol($this.Height, $this.Width)))
+        }
+        return $this.Codes[[int]$c]
+    }
+
+    static [int64]GetUnknownSymbol([int]$height, [int]$width)
+    {
+        $TopRow = (1 -shl ($width - 1)) - 1
+        $MiddleRow = (1 -shl ($width - 2)) -bor 1
+        [int64]$out = 0
+        for ($i = 0; $i -lt $height; $i++)
+        {
+            $value = $MiddleRow
+            if (($i - $height + 1) % ($height - 1) -eq 0) # true for first and last
+            {
+                $value = $TopRow
+            }
+            $shiftAmount = ($height - $i - 1) * ($width)
+            $out = $out -bor ($value -shl $shiftAmount)
+        }
+        return $out
+    }
+
+    [int64]GetColumnMask()
+    {
+        return ColumnMaskCalc($this.Height, $this.Width)
+    }
+    static [int64]ColumnMaskCalc([int]$height, [int]$width)
+    {
+        [int64]$out = 0
+        for ($i = 0; $i -lt $height; $i++)
+        {
+            [int64]$start = 1 -shl ($width - 1)
+            $Shifted = ($start -shl ($i * $width))
+            $out = $out -bor $Shifted
+
+        }
+        return $out
+    }
+}
+
+
+class DefaultFont : FontBase
+{
+    DefaultFont()
+    {
+        $this.CharHeight = 5
+        $this.CharWidth = 5
+        $this.Codes = @{
+            32  = 2 -shl 25   # Space width : can be between 1 and 5.
+            33  = 34636801    # "!"
+            34  = 106070016   # """
+            35  = 179284970   # "#"
+            36  = 184170686   # "$"
+            37  = 194842995   # "%"
+            38  = 180957773   # "&"
+            39  = 68222976    # "'"
+            40  = 68225089    # "("
+            41  = 69239842    # ")"
+            42  = 185750673   # "*"
+            43  = 172129412   # "+"
+            44  = 67108898    # ","
+            45  = 100670464   # "-"
+            46  = 67108963    # "."
+            47  = 168890640   # "/"
+            48  = 140813606   # "0"
+            49  = 107022407   # "1"
+            50  = 140806287   # "2"
+            51  = 183015982   # "3"
+            52  = 142949442   # "4"
+            53  = 150222894   # "5"
+            54  = 141834534   # "6"
+            55  = 150016264   # "7"
+            56  = 140810534   # "8"
+            57  = 140811302   # "9"
+            58  = 33587201    # ":"
+            59  = 67141666    # ";"
+            60  = 101781569   # "<"
+            61  = 100892896   # "="
+            62  = 104924228   # ">"
+            63  = 140806276   # "?"
+            64  = 200007183   # "@"
+            65  = 183041585   # "A"
+            66  = 199817790   # "B"
+            67  = 141828359   # "C"
+            68  = 199804478   # "D"
+            69  = 199782943   # "E"
+            70  = 150221064   # "F"
+            71  = 183000670   # "G"
+            72  = 143965481   # "H"
+            73  = 34636833    # "I"
+            74  = 175180366   # "J"
+            75  = 143995209   # "K"
+            76  = 142876943   # "L"
+            77  = 196794033   # "M"
+            78  = 186439281   # "N"
+            79  = 183027246   # "O"
+            80  = 150256904   # "P"
+            81  = 200853443   # "Q"
+            82  = 149207369   # "R"
+            83  = 182990894   # "S"
+            84  = 200413316   # "T"
+            85  = 143959343   # "U"
+            86  = 186172740   # "V"
+            87  = 186177194   # "W"
+            88  = 185930065   # "X"
+            89  = 185929860   # "Y"
+            90  = 200347935   # "Z"
+            91  = 70322243    # "["
+            92  = 184815681   # "\"
+            93  = 70288419    # "]"
+            94  = 102924288   # "^"
+            95  = 134217743   # "_"
+            96  = 69238784    # "`"
+            97  = 199278127   # "a"
+            98  = 185550398   # "b"
+            99  = 183026222   # "c"
+            100 = 169330223  # "d"
+            101 = 183041551  # "e"
+            102 = 184054288  # "f"
+            103 = 183024702  # "g"
+            104 = 185550385  # "h"
+            105 = 34604065   # "i"
+            106 = 168854590  # "j"
+            107 = 186217041  # "k"
+            108 = 185090575  # "l"
+            109 = 178968245  # "m"
+            110 = 199804465  # "n"
+            111 = 183027246  # "o"
+            112 = 199804880  # "p"
+            113 = 184075745  # "q"
+            114 = 191676944  # "r"
+            115 = 184039486  # "s"
+            116 = 185483838  # "t"
+            117 = 186173037  # "u"
+            118 = 186165572  # "v"
+            119 = 190502570  # "w"
+            120 = 186169905  # "x"
+            121 = 186170430  # "y"
+            122 = 200347935  # "z"
+            123 = 103878723  # "{"
+            124 = 34636833   # "|"
+            125 = 107021382  # "}"
+            126 = 139788288  # "~"
+            127 = 150250799  # "" Used as the "unknown character" symbol
+            128 = 150223247  # "€"
+        }
+    }
+}
+
 function GetLetterRow
 {
     <#
@@ -163,8 +359,8 @@ function GetLetterRow
         $Row,
 
         # The Dictionary to use as the lookup for character data
-        [System.Collections.IDictionary]
-        $Dictionary = $Codes,
+        [FontBase]
+        $Font = [DefaultFont]::new(),
 
         #The character to use as the output. Defaults to the hash sign '#'
         [char]
@@ -183,27 +379,25 @@ function GetLetterRow
     }
     Process
     {
-        if (!$Dictionary.Contains([int]$Char))
-        {
-            $Code = 150250799
-        }
-        else
-        {
-            $Code = $Dictionary[[int]$Char]
-        }
+        $CharInfo = $Font.GetCharInfo($Char)
         
-        $Width = ($Code -band (7 -shl 25)) -shr 25
-        
-        
+        $Code = $CharInfo.Value
+        $Width = $CharInfo.Width
+        # The bit mask to extract a row for a font of this width
+        $Mask = (1 -shl $Width) - 1
+        # Represents the max shift that would be required for a font of this height and width
+        $MaxShift = ($Font.Height - 1) * $Font.Width
         
         ForEach ($r in $row)
         {
+            if ($r -ge $Font.Height) {continue}
+
             $OutChars = [string]::new($EmptyChar, $Width).ToCharArray()
             # Shift the bits for the required row to the least significant position and zero the remaining bits
-            $5Bits = ($Code -shr (20 - 5 * $r)) -band 31
+            $RowBits = ($Code -shr ($MaxShift - $Width * $r)) -band $Mask
             if ($AsByte)
             {
-                $5Bits
+                $RowBits
                 continue;
             }
             $i = $OutChars.GetUpperBound(0)
@@ -236,39 +430,36 @@ function GetLetterColumn
         [char]$Character,
 
         # The Dictionary to use as the lookup for character data
-        [System.Collections.IDictionary]
-        $Dictionary = $Codes
+        [FontBase]
+        $Font = [DefaultFont]::new()
     )
     Begin
     {
-        $CharHeight = 5
+        $CharHeight = $Font.Height
     }
     Process
     {
         ForEach ($char in $Character)
         {
-            if (!$Dictionary.Contains([int]$Char))
-            {
-                $Code = 150250799
-            }
-            else
-            {
-                $Code = $Dictionary[[int]$Char]
-            }
-            $Width = ($Code -band (7 -shl 25)) -shr 25
+            $CharInfo = $Font.GetCharInfo($char)
+
+            $Code = $CharInfo.Value
+            $Width = $CharInfo.Width
 
             # Mask where all the 'a' bits (left column of character) are 1
             # We then shift this right to access the other columns
-            # 0000 0001 0000 1000 0100 0010 0001 0000
-            $ColumnMask = 17318416
+            # e.g. 0000 0001 0000 1000 0100 0010 0001 0000
+            $ColumnMask = $Font.GetColumnMask()
 
             # Shift the mask to the first column as specified by the width value:
-            $InitialShift = (5 - $Width)
+            $InitialShift = ($Font.Width - $Width)
             Write-Debug "InitialShift: $InitialShift"
 
             $ColumnMask = $ColumnMask -shr $InitialShift
             
             Write-Debug "ColumnMask: $ColumnMask"
+
+            $SlideAmount = $Font.Width - 1
 
             # The amount to bump the bits to the right so that the first value is in the 2^0 position
             $NormalisingShift = $Width - 1
@@ -281,7 +472,7 @@ function GetLetterColumn
                 #Extraction Loop
                 for ($j = 0; $j -lt $CharHeight; $j++)
                 {
-                    $ColVal += ($Normalised -shr (4 * $j)) -band (1 -shl $j)
+                    $ColVal += ($Normalised -shr ($SlideAmount * $j)) -band (1 -shl $j)
                 }
 
                 $ColVal
@@ -626,4 +817,3 @@ function Write-ScrollText
     }
 }
 
-Export-ModuleMember -Function *-*
