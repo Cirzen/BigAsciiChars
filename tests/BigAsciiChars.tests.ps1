c32426a844671f0ad5f6c25d1984626572ceeb06
David Johnson <davidcafemocha@gmail.com>
2019-10-14 14:46:35 +0100

    Neaten tests

--- tests/BigAsciiChars.tests.ps1
+++ tests/BigAsciiChars.tests.ps1
@@ -1,36 +1,30 @@
 #region Header, Unit and Module testing
 $ModuleName = "BigAsciiChars"
 
-$ModuleMetaDataPath = join-path -path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath "BigAsciiChars.psd1"
+$ModuleMetaDataPath = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath "BigAsciiChars.psd1"
 
 # Remove any module instance
 
-Get-Module -Name $ModuleName | remove-module -ErrorAction SilentlyContinue
+Get-Module -Name $ModuleName | Remove-Module -ErrorAction SilentlyContinue
 
 $ImportedModule = Import-Module -Name $ModuleMetaDataPath -Force -PassThru
 
 
 
 
-Describe "$ModuleName Manifest Testing"{
-
-
+Describe "$ModuleName Manifest Testing" {
 
 
     Context "$ModuleName Module manifest" {
 
 
         It "Should contains RootModule" {
-            $ImportedModule.RootModule | Should not BeNullOrEmpty
+            $ImportedModule.RootModule | Should -Not -BeNullOrEmpty
         }
 
         It "Should contains Author" {
             $ImportedModule.Author | Should -Not -BeNullOrEmpty
         }
-
-        It "Should contains Company Name" {
-             $ImportedModule.CompanyName|Should -Not -BeNullOrEmpty
-            }
 
         It "Should contains Description" {
             $ImportedModule.Description | Should -Not -BeNullOrEmpty
@@ -49,58 +43,67 @@ Describe "$ModuleName Manifest Testing"{
         }
 
         It "Should have at least one tag" {
-            $ImportedModule.tags.count | Should -BeGreaterThan 0
+            $ImportedModule.Tags.Count | Should -BeGreaterThan 0
         }
 
     }
+
     InModuleScope $ModuleName {
-        Context "$($ModuleName) Cmdlet testing"  {
+        Context "$($ModuleName) Cmdlet testing" {
 
-            it "Write-BigText Should return the correct length for A" {
-                (Write-BigText -Text "A").length   | Should -be 5
+            Describe "Write-BigText" {
+                
+                It "Write-BigText Should return the correct length for A" {
+                    (Write-BigText -Text "A").length | Should -Be 5
+                }
+    
+                It "Write-BigText Should throw with an empty string" {
+                    { Write-BigText -Text "" } | Should -Throw 
+                }
+    
+                It "Write-BigText Should throw with a null value" {
+                    { Write-BigText -Text $null } | Should -Throw 
+                }
             }
 
-            it "Write-BigText Should trhow with an empty string" {
-                { Write-BigText -Text "" }   | Should -Throw 
+            Describe "GetLetterColumn" {
+                
+                It "GetLetterColumn Should return a byte array" {
+                    (GetLetterColumn -Character "a") | Should -BeOfType ([byte[]])
+                }
+    
+                It "GetLetterColumn Should return an array with 6 items" {
+                    (GetLetterColumn -Character "a").Count | Should -Be 6
+                }
+    
+                It "GetLetterColumn Should throw if using a string" {
+                    { GetLetterColumn -Character "aa" } | Should -Throw 
+                }
             }
 
-            it "Write-BigText Should trhow with a null value" {
-                { Write-BigText -Text $null }    | Should -Throw 
-            }
+            Describe "GetCharWidth" {
+                
+                It "GetCharWidth Should throw if using a string" {
+                    { GetCharWidth -Char "aa" } | Should -Throw 
+                }
+    
+                It "GetCharWidth Should return an int32 value" {
+                    (GetCharWidth -Char "a") | Should -BeOfType ([int])
+                }
+    
+                It "GetCharWidth Should return 5 for the letter a" {
+                    GetCharWidth -Char "a" | Should -Be 5
+                }
+    
+                It "GetCharWidth Should return 3 for the char -" {
+                    GetCharWidth -Char "-" | Should -Be 3
+                }
 
-            it "GetLetterColumn Should return an array of Object" {
-                (GetLetterColumn -Character "a").getType().Name  | Should -Be 'Object[]'
-            }
-
-            it "GetLetterColumn Should return an array with 6 member" {
-                (GetLetterColumn -Character "a").count  | Should -Be 6
-            }
-
-            it "GetLetterColumn Should throw if using a string" {
-                { GetLetterColumn -Character "aa" } | Should -Throw 
-            }
-
-            it "GetCharWidth Should throw if using a string" {
-                { GetCharWidth -Char "aa" } | Should -Throw 
-            }
-
-            it "GetCharWidth Should return an int32 value" {
-                (GetCharWidth -Char  "a").getType().Name  | Should -Be 'int32'
-            }
-
-            it "GetCharWidth Should return 5 for the letter a" {
-                GetCharWidth -Char  "a" | Should -Be 5
-            }
-
-            it "GetCharWidth Should return 3 for the char -" {
-                GetCharWidth -Char  "-" | Should -Be 3
-            }
-
-            it "GetCharWidth Should throw if using a string" {
-                { GetCharWidth -Char "aa" } | Should -Throw 
+                It "GetCharWidth Should return 1 for the char i" {
+                    GetCharWidth -Char "i" | Should -Be 1
+                }
             }
         }
-
     }
 
 
